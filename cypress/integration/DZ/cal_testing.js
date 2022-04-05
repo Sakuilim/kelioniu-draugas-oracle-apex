a3b78081a7375745de91928dd913e50d28fcbf05
dziugito <93382954+dziugito@users.noreply.github.com>
2022-04-05 18:21:45 +0300

    added test file for calendar

---
+++ cypress/integration/DZ/cal_testing.js
@@ -0,0 +1,20 @@
+context('Unit testing APEX', () => {
+  const loginPage  = 'https://apex.oracle.com/pls/apex/idework/r/game-shop/login?session=115330830766114'
+  const pUsername   = 'admin'
+  const pPassword   = 'admin'
+  var   loggedInPage
+  var   app_cookie
+  var   pageUrl
+ 
+  it('Visit the page', () => {
+    // Arrange
+    cy.visit(loginPage)
+    cy.get('[name^=P9999_USERNAME]').type(pUsername)
+    cy.get('[name^=P9999_PASSWORD]').type(pPassword)
+    cy.contains('Sign In').click()
+    cy.contains('Calendar').click()
+    // - visit a web page
+   
+    
+  })
+})
