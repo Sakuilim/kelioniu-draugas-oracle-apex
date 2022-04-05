657edc25eaccc62a17fa5a435c7f2eb7df19d937
Oskaras <74023964+dawgoz@users.noreply.github.com>
2022-04-05 18:09:30 +0300

    Added sys-admin-test_1.js test file

---
+++ cypress/integration/OS/sys-admin-test_1.js
@@ -0,0 +1,19 @@
+
+context('Unit testing APEX', () => {
+  const loginPage  = 'https://apex.oracle.com/pls/apex/idework/r/game-shop/'
+  const pUsername   = 'admin'
+  const pPassword   = 'admin'
+  var   loggedInPage
+  var   app_cookie
+  var   pageUrl
+ 
+  it('Visit the page', () => {
+    cy.visit(loginPage)
+    cy.get('[name^=P9999_USERNAME]').type(pUsername)
+    cy.get('[name^=P9999_PASSWORD]').type(pPassword)
+    cy.contains('Sign In').click()
+    cy.contains('Map').click()
+ 
+    cy.frameLoaded()
+  })
+})
