f3621e550b62a4315b709d8b82576e87f45539bc
Kudzhma <kudzmabenas@gmail.com>
2022-04-06 10:59:32 +0300

    Update speed-test.spec.js

--- cypress/integration/unit-tests/1-kelioniu-draugas/BK/speed-test.spec.js
+++ cypress/integration/unit-tests/1-kelioniu-draugas/BK/speed-test.spec.js
@@ -5,7 +5,7 @@ context('Unit testing APEX', () => {
     var   loggedInPage
     var   app_cookie
     var   pageUrl
-   
+    
     it('Visit the page', () => {
       // Arrange
       cy.viewport('macbook-16')
@@ -14,7 +14,7 @@ context('Unit testing APEX', () => {
       cy.get('[name^=P9999_PASSWORD]').type(pPassword)
       cy.contains('Sign In').click()
       cy.contains('Base Data').click()
-      // ----------------------------
+      // ---------------------------- Test to save data
       // TEST #3
       // Act
       cy.get('#R54885544083813645701_ig_toolbar_actions_button > .a-Icon').click()
