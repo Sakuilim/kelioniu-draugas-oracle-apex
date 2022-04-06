1af5dbbd139e89c68f990b8952e3ac0fca821867
Kudzhma <kudzmabenas@gmail.com>
2022-04-06 04:29:14 +0300

    Create speed-test.spec.js
    PKPRIF-9 Paieškos funkcijų automatinis testavimas

---
+++ cypress/integration/unit-tests/1-kelioniu-draugas/BK/speed-test.spec.js
@@ -0,0 +1,46 @@
+
+context('Unit testing APEX', () => {
+    const loginPage  = 'https://apex.oracle.com/pls/apex/idework/r/game-shop/prisijungimas?session=16779305017243'
+    const pUsername   = 'admin'
+    const pPassword   = 'admin'
+    var   loggedInPage
+    var   app_cookie
+    var   pageUrl
+   
+    it('Visit the page', () => {
+      // Arrange
+      cy.viewport('macbook-16')
+      cy.visit(loginPage)
+      cy.get('[name^=P9999_USERNAME]').type(pUsername)
+      cy.get('[name^=P9999_PASSWORD]').type(pPassword)
+      cy.contains('Sign In').click()
+      cy.contains('Base Data').click()
+      // ----------------------------
+      // TEST #1
+      // Act
+      cy.get('#R54885544083813645701_ig_toolbar_search_field').type('Puntukas').click(cy.get('.a-Toolbar-group--search > .js-actionButton > .a-Button-label'))
+      // Assert
+      cy.wait(2000)
+      cy.get('.a-Toolbar-groupContainer--end > .a-Toolbar-group > .a-Button > .a-Button-label').click()
+      // ----------------------------
+      // TEST #2
+      // Act
+      //cy.get('#R54885544083813645701_ig_toolbar_actions_button > .a-Icon').click()
+      //cy.get('#R54885544083813645701_ig_toolbar_3i').click()
+      //cy.get('#R54885544083813645701_ig_toolbar_3_0').click()
+      //cy.get('#R54885544083813645701_ig_SD_COLUMN').select('54885544424232645705')
+      //cy.get('.ui-button--hot').click()
+      // Assert
+      //cy.wait(2000)
+      //cy.get('.a-Toolbar-groupContainer--end > .a-Toolbar-group > .a-Button > .a-Button-label').click()
+      // ----------------------------
+      // TEST #3
+      // Act
+      //cy.get('#R54885544083813645701_ig_toolbar_actions_button > .a-Icon').click()
+      //cy.get('#R54885544083813645701_ig_toolbar_11').click()
+      //cy.get('[data-value="PDF"] > .a-IG-dialog-iconList-link > .a-IG-dialog-iconList-label').click()
+      //cy.get('.ui-button--hot').click()
+      // Assert
+      //cy.wait(2000)
+    })
+  })
